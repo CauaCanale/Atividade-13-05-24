c34029af60d8135e68d224e5ef3ba8d66322c37d
CauaCanale <caua.cferreira2@gmail.com>
2024-05-24 12:57:01 -0300

    Add files via upload

---
+++ Primeira aula 13.05.24/main.js
@@ -0,0 +1,39 @@
+//variavel é usada como algo que muda
+console.log("olá mundo")
+ 
+//texto -> string (guarda informaçoes)
+//numeros inteiros -> int (armazenas inteiros 1,2,3)
+//numeros decimais -> floot / double (floot guarda menos/double guarda mais informaçoes )
+//logicos -> boolean (representa o sim/não) em ingles seria (true or false)
+ 
+  //  var telefone = "19 991486337" // as ("") significa que é uma string
+  //  var valormonetario = 50.59
+  //  var valorinteiro = 13
+  //  var truefalse = true
+ 
+//texto -> Guarde os dados do seu endereço em VARIAVEIS (ex. rua, bairro, cep, estado...)
+// Guarde os seus dados (nome, email, telefone, data de nascimento,genero) em VARIAVEIS
+//Crie uma VARIAVEL que diz que se voce é maior de idade
+//Crie uma VARIAVEL que diz se você pode dirigir
+ 
+ 
+var rua = "Itapemirm"
+var bairro = "Jardim ipiranga,"
+var cep = "113468480,"
+var estado = "São Paulo."
+     console.log ("")
+console.log (rua, bairro, cep, estado)
+ 
+var  nome = "Cauã Canale"
+var  email = "caua.cferreira2@gmail.com"
+var  telefone = "19 991486337"
+var  genero = "Macho"
+ 
+     console.log (nome)
+     console.log (email)
+     console.log (telefone)
+     console.log (genero)
+     console.log ("")
+
+     var  maior_de_idade = "false"
+ console.log (maior_de_idade)
